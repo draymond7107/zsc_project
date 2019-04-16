7d1fd2655eef0f38d66ffaaefa1a6a98cd62dd87
张苏超 <draymond7107@sina.com>
2019-04-16 09:45:34 +0800

    钉钉对接 部分功能

---
+++ src/main/java/com/zsc/tbpractice/http/dingding/Login.java
@@ -0,0 +1,25 @@
+package com.zsc.tbpractice.http.dingding;
+
+
+import com.zsc.base.abs.BaseController;
+import com.zsc.base.spring.JsonResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ZhangSuchao
+ * @create 2019/4/16
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/dingding/login")
+public class Login extends BaseController {
+
+    public JsonResult login() {
+
+
+        return null;
+    }
+
+
+}
