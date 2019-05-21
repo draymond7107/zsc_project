52c5bd5685452dae97503f7a3888f5be3daf1227
张苏超 <draymond7107@sina.com>
2019-05-21 20:47:51 +0800

    线程基础

---
+++ src/main/java/com/zsc/tbpractice/multiThread/MyRunnable.java
@@ -0,0 +1,25 @@
+package com.zsc.tbpractice.multiThread;
+
+
+/**
+ * @author ZhangSuchao
+ * @create 2019/5/21
+ * @since 1.0.0
+ */
+
+public class MyRunnable implements Runnable {
+    int i = 0;      //i写到对象里与写到方法里得到不同的结果
+
+    @Override
+    public void run() {
+
+        int sum = 0;
+
+        while (this.i <= 10) {
+            sum += i;
+            i++;
+            System.out.println(Thread.currentThread().getName() + " ===" + sum);
+        }
+
+    }
+}
